a1140d1aabe7e60efeec60664ffce7d26e3104af
Peter Stromquist <peter@twistedstream.com>
2011-02-28 19:28:20 -0600

    Added XML documetation to the NodeModel and FilteredNodeModel DTO classes

--- src/FluentSiteMap/FilteredNodeModel.cs
+++ src/FluentSiteMap/FilteredNodeModel.cs
@@ -3,13 +3,36 @@ using System.Diagnostics;
 
 namespace FluentSiteMap
 {
+    /// <summary>
+    /// Represents the state of a site map node after it has been built and filtered.
+    /// </summary>
     [DebuggerDisplay("Title = {Title}, Url = {Url}, IsCurrent = {IsCurrent}")]
     public class FilteredNodeModel
     {
+        /// <summary>
+        /// Gets or sets the node title.
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the node description.
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the node URL.
+        /// </summary>
         public string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether the node is the current node 
+        /// relative to the current HTTP request.
+        /// </summary>
         public bool IsCurrent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the children nodes.
+        /// </summary>
         public IList<FilteredNodeModel> Children { get; set; }
     }
 }
--- src/FluentSiteMap/NodeModel.cs
+++ src/FluentSiteMap/NodeModel.cs
@@ -3,19 +3,45 @@ using System.Diagnostics;
 
 namespace FluentSiteMap
 {
+    /// <summary>
+    /// Represents the state of a site map node after it has been built, 
+    /// but before it has been filtered.
+    /// </summary>
     [DebuggerDisplay("Title = {Title}, Url = {Url}")]
     public class NodeModel
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NodeModel"/> class.
+        /// </summary>
         public NodeModel()
         {
             Children = new List<NodeModel>();
             Filters = new List<INodeFilter>();
         }
 
+        /// <summary>
+        /// Gets or sets the node title.
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the node description.
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the node URL.
+        /// </summary>
         public string Url { get; set; }
+
+        /// <summary>
+        /// Gets the children nodes.
+        /// </summary>
         public IList<NodeModel> Children { get; internal set; }
+
+        /// <summary>
+        /// Gets or sets the filters to be applied to this node during filtering.
+        /// </summary>
         internal IList<INodeFilter> Filters { get; set; }
     }
 }
