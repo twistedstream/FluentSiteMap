a07907881701483b91536b065697c7e5a8d16cb9
Peter Stromquist <peter@twistedstream.com>
2011-03-02 08:03:59 -0600

    Intermediate commit

---
+++ src/FluentSiteMap.Test/BuilderContextTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace FluentSiteMap.Test
+{
+    [TestFixture]
+    public class BuilderContextTests
+        : TestBase
+    {
+        [Test]
+        public void Class_should_require_a_request_context()
+        {
+            
+        }
+    }
+}
--- src/FluentSiteMap/BuilderContext.cs
+++ src/FluentSiteMap/BuilderContext.cs
@@ -4,13 +4,29 @@ using System.Web.Routing;
 
 namespace FluentSiteMap
 {
+    /// <summary>
+    /// Carries state during a site map build process.
+    /// </summary>
     public class BuilderContext
     {
         private readonly IDictionary<string, string> _metadata = new Dictionary<string, string>();
 
+        /// <summary>
+        /// Gets the <see cref="RequestContext"/> associated with the site map build.
+        /// </summary>
         public RequestContext RequestContext { get; private set; }
+
+        /// <summary>
+        /// Gets the parent <see cref="BuilderContext"/> instance, if one exists.
+        /// </summary>
         public BuilderContext Parent { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of a root <see cref="BuilderContext"/>.
+        /// </summary>
+        /// <param name="requestContext">
+        /// The <see cref="RequestContext"/> associated with the current HTTP request.
+        /// </param>
         public BuilderContext(RequestContext requestContext)
         {
             if (requestContext == null) throw new ArgumentNullException("requestContext");
@@ -18,6 +34,12 @@ namespace FluentSiteMap
             RequestContext = requestContext;
         }
 
+        /// <summary>
+        /// Initializes a new instance of a child <see cref="BuilderContext"/>.
+        /// </summary>
+        /// <param name="parent">
+        /// The parent <see cref="BuilderContext"/> instance.
+        /// </param>
         public BuilderContext(BuilderContext parent)
             : this(parent.RequestContext)
         {
@@ -26,6 +48,11 @@ namespace FluentSiteMap
             Parent = parent;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public string GetMetadata(string key)
         {
             // recursively search for the metadata value
