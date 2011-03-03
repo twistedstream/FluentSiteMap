1185f9ee2a528531b6f57e72d481502dd8fa4773
Peter Stromquist <peter@twistedstream.com>
2011-03-03 16:40:54 -0600

    Added XML documentation for the INodeFilter interface

--- src/FluentSiteMap/INodeFilter.cs
+++ src/FluentSiteMap/INodeFilter.cs
@@ -1,7 +1,23 @@
 ﻿namespace FluentSiteMap
 {
+    /// <summary>
+    /// Filteres the existing state or existance of a site map node.
+    /// </summary>
     public interface INodeFilter
     {
+        /// <summary>
+        /// Filters a site map node.
+        /// </summary>
+        /// <param name="node">
+        /// The <see cref="FilteredNodeModel"/> instance whose state can be filtered (modified).
+        /// </param>
+        /// <param name="context">
+        /// A <see cref="FilterContext"/> instance used when filtering the node.
+        /// </param>
+        /// <returns>
+        /// False if the node is supposed to be entirely removed from the final filtered site map; 
+        /// otherwise true.
+        /// </returns>
         bool Filter(FilteredNodeModel node, FilterContext context);
     }
 }
