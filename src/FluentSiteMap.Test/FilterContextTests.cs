3719b18112dd34a76d79b30f626fe182388c0ed5
Peter Stromquist <peter@twistedstream.com>
2011-03-02 23:08:37 -0600

    Added tests and XML documentation for the FilterContext class.

---
+++ src/FluentSiteMap.Test/FilterContextTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Routing;
+using NUnit.Framework;
+
+namespace FluentSiteMap.Test
+{
+    [TestFixture]
+    public class FilterContextTests
+        : TestBase
+    {
+        [Test]
+        public void Instances_should_require_a_request_context()
+        {
+            // Arrange
+            RequestContext requestContext = null;
+
+            // Act
+            var ex = Assert.Throws<ArgumentNullException>(
+                () => new FilterContext(requestContext));
+
+            // Assert
+            Assert.That(ex.ParamName, Is.EqualTo("requestContext"));
+        }
+
+        [Test]
+        public void Instances_should_set_the_request_context()
+        {
+            // Arrange
+            var requestContext = new RequestContext();
+
+            // Act
+            var target = new FilterContext(requestContext);
+
+            // Assert
+            Assert.That(target.RequestContext, Is.EqualTo(requestContext));
+        }
+    }
+}
--- src/FluentSiteMap/FilterContext.cs
+++ src/FluentSiteMap/FilterContext.cs
@@ -3,10 +3,22 @@ using System.Web.Routing;
 
 namespace FluentSiteMap
 {
+    /// <summary>
+    /// Carries state during a site map filter process.
+    /// </summary>
     public class FilterContext
     {
+        /// <summary>
+        /// Gets the <see cref="RequestContext"/> associated with the site map filter.
+        /// </summary>
         public RequestContext RequestContext { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FilterContext"/> class.
+        /// </summary>
+        /// <param name="requestContext">
+        /// The <see cref="RequestContext"/> associated with the current HTTP request.
+        /// </param>
         public FilterContext(RequestContext requestContext)
         {
             if (requestContext == null) throw new ArgumentNullException("requestContext");
