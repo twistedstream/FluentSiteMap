4a5985d20f94774848f62836727b8f421d3df61d
Peter Stromquist <peter@twistedstream.com>
2011-03-03 16:12:33 -0600

    Added tests and XML documentation for the DecoratingNodeBuilder class

---
+++ src/FluentSiteMap.Test/DecoratingNodeBuilderTests.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Routing;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace FluentSiteMap.Test
+{
+    [TestFixture]
+    public class DecoratingNodeBuilderTests
+        : TestBase
+    {
+        private INodeBuilder _inner;
+        private BuilderContext _context;
+        private NodeModel _node;
+
+        public override void Setup()
+        {
+            base.Setup();
+
+            _inner = MockRepository.GenerateStub<INodeBuilder>();
+            _context = new BuilderContext(new RequestContext());
+            _node = new NodeModel(new List<INodeFilter>());
+        }
+
+        [Test]
+        public void Instances_should_require_an_inner_builder()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(
+                () => new TestDecoratingNodeBuilder(null));
+
+            Assert.That(ex.ParamName, Is.EqualTo("inner"));
+        }
+
+        [Test]
+        public void Filters_should_return_the_inner_filters()
+        {
+            // Arrange
+            var filters = new[] {MockRepository.GenerateStub<INodeFilter>()};
+
+            _inner
+                .Stub(i => i.Filters)
+                .Return(filters);
+
+            var target = new TestDecoratingNodeBuilder(_inner);
+
+            // Act
+            var result = target.Filters;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(filters));
+        }
+
+        [Test]
+        public void Build_should_require_a_context()
+        {
+            // Arrange
+            var target = new TestDecoratingNodeBuilder(_inner);
+
+            // Act
+            var ex = Assert.Throws<ArgumentNullException>(
+                () => target.Build(null));
+
+            Assert.That(ex.ParamName, Is.EqualTo("context"));
+        }
+
+        [Test]
+        public void Build_should_call_the_inner_Build()
+        {
+            // Arrange
+            _inner = MockRepository.GenerateMock<INodeBuilder>();
+
+            var target = new TestDecoratingNodeBuilder(_inner);
+
+            // Act
+            target.Build(_context);
+
+            // Assert
+            _inner.AssertWasCalled(i => i.Build(_context));
+        }
+
+        [Test]
+        public void Build_should_call_OnBuild()
+        {
+            // Arrange
+            _inner
+                .Stub(i => i.Build(_context))
+                .Return(_node);
+
+            var target = new TestDecoratingNodeBuilder(_inner);
+
+            // Act
+            target.Build(_context);
+
+            // Assert
+            Assert.That(target.OnBuildNode, Is.EqualTo(_node));
+            Assert.That(target.OnBuildContext, Is.EqualTo(_context));
+        }
+
+        [Test]
+        public void Build_should_return_the_resulting_node()
+        {
+            // Arrange
+            _inner
+                .Stub(i => i.Build(_context))
+                .Return(_node);
+
+            var target = new TestDecoratingNodeBuilder(_inner);
+
+            // Act
+            var result = target.Build(_context);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(_node));
+        }
+
+        private class TestDecoratingNodeBuilder
+            : DecoratingNodeBuilder
+        {
+            public NodeModel OnBuildNode { get; private set; }
+            public BuilderContext OnBuildContext { get; private set; }
+
+            public TestDecoratingNodeBuilder(INodeBuilder inner) : base(inner) {}
+
+            protected override void OnBuild(NodeModel node, BuilderContext context)
+            {
+                OnBuildNode = node;
+                OnBuildContext = context;
+            }
+        }
+    }
+}
