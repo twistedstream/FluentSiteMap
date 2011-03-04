5b41c6beb095eecb127bb05cb880d3f75b7dd791
Peter Stromquist <peter@twistedstream.com>
2011-03-03 21:17:52 -0600

    Added DescriptionNodeBuilder / WithDescriptionExtensions classes

---
+++ src/FluentSiteMap/Builders/DescriptionNodeBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FluentSiteMap.Builders
+{
+    public class DescriptionNodeBuilder
+        : DecoratingNodeBuilder
+    {
+        private readonly Func<NodeModel, string> _descriptionGenerator;
+
+        public DescriptionNodeBuilder(INodeBuilder inner, Func<NodeModel, string> descriptionGenerator)
+            : base(inner)
+        {
+            if (descriptionGenerator == null) throw new ArgumentNullException("descriptionGenerator");
+
+            _descriptionGenerator = descriptionGenerator;
+        }
+
+        protected override void OnBuild(NodeModel node, BuilderContext context)
+        {
+            node.Description = _descriptionGenerator(node);
+        }
+    }
+}
---
+++ src/FluentSiteMap/Builders/WithDescriptionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FluentSiteMap.Builders
+{
+    public static class WithDescriptionExtensions
+    {
+        public static INodeBuilder WithDescription(this INodeBuilder inner, Func<NodeModel, string> descriptionGenerator)
+        {
+            return new DescriptionNodeBuilder(inner, descriptionGenerator);
+        }
+
+        public static INodeBuilder WithDescription(this INodeBuilder inner, string description)
+        {
+            return WithDescription(inner, n => description);
+        }
+    }
+}
