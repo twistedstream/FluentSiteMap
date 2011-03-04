37b239fcca6289a59e26e7abe499e90b62963dc0
Peter Stromquist <peter@twistedstream.com>
2011-03-03 21:21:54 -0600

    Consolidated all builder extension methods into BuilderExtensions Consolidated all filter extension methods into FilterExtensions

---
+++ src/FluentSiteMap/Builders/BuilderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FluentSiteMap.Builders
+{
+    public static class BuilderExtensions
+    {
+        public static INodeBuilder WithTitle(this INodeBuilder inner, Func<NodeModel, string> titleGenerator)
+        {
+            return new TitleNodeBuilder(inner, titleGenerator);
+        }
+
+        public static INodeBuilder WithTitle(this INodeBuilder inner, string title)
+        {
+            return WithTitle(inner, n => title);
+        }
+
+        public static INodeBuilder WithDescription(this INodeBuilder inner, Func<NodeModel, string> descriptionGenerator)
+        {
+            return new DescriptionNodeBuilder(inner, descriptionGenerator);
+        }
+
+        public static INodeBuilder WithDescription(this INodeBuilder inner, string description)
+        {
+            return WithDescription(inner, n => description);
+        }
+
+        public static INodeBuilder WithUrlFrom(this INodeBuilder inner, IUrlProvider urlProvider)
+        {
+            return new UrlFromProviderNodeBuilder(inner, urlProvider);
+        }
+
+        public static INodeBuilder WithChildren(this INodeBuilder inner, params INodeBuilder[] childBuilders)
+        {
+            return new ChildrenNodeBuilder(inner, childBuilders);
+        }
+    }
+}
--- src/FluentSiteMap/Filters/FilterExtensions.cs
+++ src/FluentSiteMap/Filters/FilterExtensions.cs
@@ -2,8 +2,14 @@
 
 namespace FluentSiteMap.Filters
 {
-    public static class TitleFilterExtensions
+    public static class FilterExtensions
     {
+        public static INodeBuilder FilteredByRoles(this INodeBuilder builder, params string[] roles)
+        {
+            builder.Filters.Add(new RolesNodeFilter(roles));
+            return builder;
+        }
+
         public static INodeBuilder FilteredTitle(this INodeBuilder builder, Func<string, string> title)
         {
             builder.Filters.Add(new TitleFilter(title));
