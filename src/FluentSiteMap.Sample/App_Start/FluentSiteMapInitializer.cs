cd8cbd3c3d306bd64e30fbb701a31ed4ea01f7f1
Peter Stromquist <peter@twistedstream.com>
2013-03-18 07:14:12 -0500

    Rename FluentSiteMapCreator to *Initializer in Sample
    To match package

--- src/FluentSiteMap.Sample/App_Start/FluentSiteMapInitializer.cs
+++ src/FluentSiteMap.Sample/App_Start/FluentSiteMapInitializer.cs
@@ -1,13 +1,14 @@
 using WebActivator;
 using TS.FluentSiteMap.Sample.Models;
 
-[assembly: PostApplicationStartMethod(typeof(TS.FluentSiteMap.Sample.App_Start.FluentSiteMapCreator), "PostStart")]
+[assembly: PostApplicationStartMethod(typeof(TS.FluentSiteMap.Sample.App_Start.FluentSiteMapInitializer), "Initialize")]
 
 namespace TS.FluentSiteMap.Sample.App_Start
 {
-    public static class FluentSiteMapCreator
+    public static class FluentSiteMapInitializer
     {
-        public static void PostStart() {
+        public static void Initialize()
+        {
             var siteMap = new MySiteMap(new ProductRepository());
             SiteMapHelper.RegisterRootSiteMap(siteMap);
         }
